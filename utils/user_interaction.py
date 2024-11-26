c7fb2a3f3a758268c85695a59fd6fe68af7f2a05
LoewiD <dennis.timlo@gmail.com>
2024-11-26 08:35:27 +0100

    Update user_interaction.py

--- utils/user_interaction.py
+++ utils/user_interaction.py
@@ -23,3 +23,5 @@ def get_user_input():
     else:
         st.error("No ingredients available to select.") # ensure that we have a return even if there is an error while fetching the data
         return [], "Don't care", "Don't care", "Don't care" # if we have an error we return an empty list and 3x don't care
+
+#hallo
